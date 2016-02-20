9f4b8b72cf8c17402a1842b645225004ada045d8
Malcolm <malcolm@robtecs.com>
2016-02-20 23:09:07 +0000

    Updated .....

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,29 @@ class UsersController < ApplicationController
         @user = User.new
     end
     
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-            redirect_to articles_path
-        else
-            render 'new'
-    
+        def create
+            @user = User.new(user_params)
+            if @user.save
+                flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
+                redirect_to articles_path
+                    else
+                    render 'new'
+            end
         end
-    end
     
+            def edit
+                @user = User.find(params[:id])    
+            end
+    
+                def update
+                    @user = User.find(params[:id])
+                    if @user.update(user_params)
+                        flash[:success] = "Your Account was updated successfully"
+                        redirect_to articles_path
+                            else
+                            render 'edit'
+                    end
+                end
     private
     
     def user_params
