cac922693ead8d0d90f8dffa3eaf4443fde140fe
Malcolm <malcolm@robtecs.com>
2016-02-23 12:34:15 +0000

    Merge Went Wrong, likly needed to overwrite....

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,7 @@ class UsersController < ApplicationController
         @user = User.new
     end
     
-<<<<<<< HEAD
-        def create
-            @user = User.new(user_params)
-            if @user.save
-                flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-                redirect_to articles_path
-                    else
-                    render 'new'
-            end
-=======
+
     def show
         @user = User.find(params[:id]) 
     end
@@ -24,29 +15,11 @@ class UsersController < ApplicationController
             redirect_to articles_path
         else
             render 'new'
-    
->>>>>>> testing-article-merge
+
         end
     
-<<<<<<< HEAD
-            def edit
-                @user = User.find(params[:id])    
-            end
-    
-                def update
-                    @user = User.find(params[:id])
-                    if @user.update(user_params)
-                        flash[:success] = "Your Account was updated successfully"
-                        redirect_to articles_path
-                            else
-                            render 'edit'
-                    end
-                end
-                    
-            def show
-                @user = User.find(params[:id])
-            end
-=======
+    end
+
     def edit
         @user = User.find(params[:id])
     end
@@ -62,7 +35,7 @@ class UsersController < ApplicationController
         end
     end
     
->>>>>>> testing-article-merge
+
     private
     
     def user_params
